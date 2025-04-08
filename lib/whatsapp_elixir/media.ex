01dd6e5b9d464348d49defd55e3efa8ae658caf2
jhonmolano09 <jhonmol007@gmail.com>
2025-04-08 13:04:33 -0500

    Refactor message sending functions to use opts for parameters and update documentation

--- lib/whatsapp_elixir/media.ex
+++ lib/whatsapp_elixir/media.ex
@@ -14,18 +14,22 @@ alias WhatsappElixir.HTTP
 Sends a location message to a WhatsApp user.
 
 ## Args
-  - lat: Latitude of the location
-  - long: Longitude of the location
-  - name: Name of the location
-  - address: Address of the location
   - recipient_id: Phone number of the user with country code without +
+  - opts: Keyword list with the following options:
+    - lat: Latitude of the location
+    - long: Longitude of the location
+    - name: Name of the location
+    - address: Address of the location
+    - custom_config: Custom configuration for HTTP client (default: [])
 
 ## Example
 
-    iex> WhatsappElixir.send_location("-23.564", "-46.654", "My Location", "Rua dois, 123", "5511999999999")
+    iex> WhatsappElixir.send_location("5511999999999", lat: "-23.564", long: "-46.654", name: "My Location", address: "Rua dois, 123")
     {:ok, %{"success" => true}}
 """
-def send_location(lat, long, name, address, recipient_id, custom_config \\ []) do
+def send_location(recipient_id, [lat: lat, long: long, name: name, address: address] = opts) do
+  custom_config = Keyword.get(opts, :custom_config, [])
+
   data = %{
     messaging_product: "whatsapp",
     to: recipient_id,
@@ -53,18 +57,25 @@ end
 Sends an image message to a WhatsApp user.
 
 ## Args
-  - image: Image id or link of the image
   - recipient_id: Phone number of the user with country code without +
-  - recipient_type: Type of the recipient, either individual or group (default is "individual")
-  - caption: Caption of the image (default is "")
-  - link: Whether to send an image id or an image link, True means that the image is a link, False means that the image is an id (default is true)
+  - opts: Keyword list with the following options:
+    - image: Image id or link of the image
+    - recipient_type: Type of the recipient, either individual or group (default: "individual")
+    - caption: Caption of the image (default: "")
+    - link: Whether to send an image id or an image link, true means that the image is a link, false means that the image is an id (default: true)
+    - custom_config: Custom configuration for HTTP client (default: [])
 
 ## Example
 
-    iex> WhatsappElixir.send_image("https://i.imgur.com/Fh7XVYY.jpeg", "5511999999999")
+    iex> WhatsappElixir.send_image("5511999999999", image: "https://i.imgur.com/Fh7XVYY.jpeg")
     {:ok, %{"success" => true}}
 """
-def send_image(image, recipient_id, custom_config \\ [], recipient_type \\ "individual", caption \\ "", link \\ true) do
+def send_image(recipient_id, [image: image] = opts) do
+  recipient_type = Keyword.get(opts, :recipient_type, "individual")
+  caption = Keyword.get(opts, :caption, "")
+  link = Keyword.get(opts, :link, true)
+  custom_config = Keyword.get(opts, :custom_config, [])
+
   data = %{
     messaging_product: "whatsapp",
     recipient_type: recipient_type,
@@ -88,17 +99,23 @@ end
 Sends a sticker message to a WhatsApp user.
 
 ## Args
-  - sticker: Sticker id or link of the sticker
   - recipient_id: Phone number of the user with country code without +
-  - recipient_type: Type of the recipient, either individual or group (default is "individual")
-  - link: Whether to send a sticker id or a sticker link, True means that the sticker is a link, False means that the sticker is an id (default is true)
+  - opts: Keyword list with the following options:
+    - sticker: Sticker id or link of the sticker
+    - recipient_type: Type of the recipient, either individual or group (default: "individual")
+    - link: Whether to send a sticker id or a sticker link, true means that the sticker is a link, false means that the sticker is an id (default: true)
+    - custom_config: Custom configuration for HTTP client (default: [])
 
 ## Example
 
-    iex> WhatsappElixir.send_sticker("170511049062862", "5511999999999", "individual", false)
+    iex> WhatsappElixir.send_sticker("5511999999999", sticker: "170511049062862", link: false)
     {:ok, %{"success" => true}}
 """
-def send_sticker(sticker, recipient_id, custom_config \\ [],  recipient_type \\ "individual", link \\ true) do
+def send_sticker(recipient_id, [sticker: sticker] = opts) do
+  recipient_type = Keyword.get(opts, :recipient_type, "individual")
+  link = Keyword.get(opts, :link, true)
+  custom_config = Keyword.get(opts, :custom_config, [])
+
   data = %{
     messaging_product: "whatsapp",
     recipient_type: recipient_type,
@@ -122,16 +139,21 @@ end
 Sends an audio message to a WhatsApp user.
 
 ## Args
-  - audio: Audio id or link of the audio
   - recipient_id: Phone number of the user with country code without +
-  - link: Whether to send an audio id or an audio link, True means that the audio is a link, False means that the audio is an id (default is true)
+  - opts: Keyword list with the following options:
+    - audio: Audio id or link of the audio
+    - link: Whether to send an audio id or an audio link, true means that the audio is a link, false means that the audio is an id (default: true)
+    - custom_config: Custom configuration for HTTP client (default: [])
 
 ## Example
 
-    iex> WhatsappElixir.send_audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", "5511999999999")
+    iex> WhatsappElixir.send_audio("5511999999999", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
     {:ok, %{"success" => true}}
 """
-def send_audio(audio, recipient_id, custom_config \\ [],   link \\ true) do
+def send_audio(recipient_id, [audio: audio] = opts) do
+  link = Keyword.get(opts, :link, true)
+  custom_config = Keyword.get(opts, :custom_config, [])
+
   data = %{
     messaging_product: "whatsapp",
     to: recipient_id,
@@ -154,17 +176,23 @@ end
 Sends a video message to a WhatsApp user.
 
 ## Args
-  - video: Video id or link of the video
   - recipient_id: Phone number of the user with country code without +
-  - caption: Caption of the video (default is "")
-  - link: Whether to send a video id or a video link, True means that the video is a link, False means that the video is an id (default is true)
+  - opts: Keyword list with the following options:
+    - video: Video id or link of the video
+    - caption: Caption of the video (default: "")
+    - link: Whether to send a video id or a video link, true means that the video is a link, false means that the video is an id (default: true)
+    - custom_config: Custom configuration for HTTP client (default: [])
 
 ## Example
 
-    iex> WhatsappElixir.send_video("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "5511999999999")
+    iex> WhatsappElixir.send_video("5511999999999", video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     {:ok, %{"success" => true}}
 """
-def send_video(video, recipient_id, custom_config \\ [],  caption \\ "", link \\ true) do
+def send_video(recipient_id, [video: video] = opts) do
+  caption = Keyword.get(opts, :caption, "")
+  link = Keyword.get(opts, :link, true)
+  custom_config = Keyword.get(opts, :custom_config, [])
+
   data = %{
     messaging_product: "whatsapp",
     to: recipient_id,
@@ -187,17 +215,23 @@ end
 Sends a document message to a WhatsApp user.
 
 ## Args
-  - document: Document id or link of the document
   - recipient_id: Phone number of the user with country code without +
-  - caption: Caption of the document (default is "")
-  - link: Whether to send a document id or a document link, True means that the document is a link, False means that the document is an id (default is true)
+  - opts: Keyword list with the following options:
+    - document: Document id or link of the document
+    - caption: Caption of the document (default: "")
+    - link: Whether to send a document id or a document link, true means that the document is a link, false means that the document is an id (default: true)
+    - custom_config: Custom configuration for HTTP client (default: [])
 
 ## Example
 
-    iex> WhatsappElixir.Media.send_document("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf", "5511999999999")
+    iex> WhatsappElixir.Media.send_document("5511999999999", document: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
     {:ok, %{"success" => true}}
 """
-def send_document(document, recipient_id, custom_config \\ [], caption \\ "", link \\ true) do
+def send_document(recipient_id, [document: document] = opts) do
+  caption = Keyword.get(opts, :caption, "")
+  link = Keyword.get(opts, :link, true)
+  custom_config = Keyword.get(opts, :custom_config, [])
+
   data = %{
     messaging_product: "whatsapp",
     to: recipient_id,
