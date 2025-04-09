dcd3d92747c9485927c22ae8d24da389034c4a0e
jhonmolano09 <jhonmol007@gmail.com>
2025-04-09 14:23:08 -0500

    Update documentation and refactor message sending functions to use maps for options

--- lib/whatsapp_elixir/media.ex
+++ lib/whatsapp_elixir/media.ex
@@ -15,7 +15,7 @@ Sends a location message to a WhatsApp user.
 
 ## Args
   - recipient_id: Phone number of the user with country code without +
-  - opts: Keyword list with the following options:
+  - opts: Map with the following options:
     - lat: Latitude of the location
     - long: Longitude of the location
     - name: Name of the location
@@ -24,11 +24,11 @@ Sends a location message to a WhatsApp user.
 
 ## Example
 
-    iex> WhatsappElixir.send_location("5511999999999", lat: "-23.564", long: "-46.654", name: "My Location", address: "Rua dois, 123")
+    iex> WhatsappElixir.send_location("5511999999999", %{lat: "-23.564", long: "-46.654", name: "My Location", address: "Rua dois, 123"})
     {:ok, %{"success" => true}}
 """
-def send_location(recipient_id, [lat: lat, long: long, name: name, address: address] = opts) do
-  custom_config = Keyword.get(opts, :custom_config, [])
+def send_location(recipient_id, %{lat: lat, long: long, name: name, address: address} = opts) do
+  custom_config = Map.get(opts, :custom_config, [])
 
   data = %{
     messaging_product: "whatsapp",
@@ -43,7 +43,7 @@ def send_location(recipient_id, [lat: lat, long: long, name: name, address: addr
   }
 
   Logger.info("Sending location to #{recipient_id}")
-  case HTTP.post(@endpoint, data, custom_config ) do
+  case HTTP.post(@endpoint, data, custom_config) do
     {:ok, response} ->
       Logger.info("Location sent to #{recipient_id}")
       response
@@ -58,7 +58,7 @@ Sends an image message to a WhatsApp user.
 
 ## Args
   - recipient_id: Phone number of the user with country code without +
-  - opts: Keyword list with the following options:
+  - opts: Map with the following options:
     - image: Image id or link of the image
     - recipient_type: Type of the recipient, either individual or group (default: "individual")
     - caption: Caption of the image (default: "")
@@ -67,14 +67,14 @@ Sends an image message to a WhatsApp user.
 
 ## Example
 
-    iex> WhatsappElixir.send_image("5511999999999", image: "https://i.imgur.com/Fh7XVYY.jpeg")
+    iex> WhatsappElixir.send_image("5511999999999", %{image: "https://i.imgur.com/Fh7XVYY.jpeg"})
     {:ok, %{"success" => true}}
 """
-def send_image(recipient_id, [image: image] = opts) do
-  recipient_type = Keyword.get(opts, :recipient_type, "individual")
-  caption = Keyword.get(opts, :caption, "")
-  link = Keyword.get(opts, :link, true)
-  custom_config = Keyword.get(opts, :custom_config, [])
+def send_image(recipient_id, %{image: image} = opts) do
+  recipient_type = Map.get(opts, :recipient_type, "individual")
+  caption = Map.get(opts, :caption, "")
+  link = Map.get(opts, :link, true)
+  custom_config = Map.get(opts, :custom_config, [])
 
   data = %{
     messaging_product: "whatsapp",
@@ -100,7 +100,7 @@ Sends a sticker message to a WhatsApp user.
 
 ## Args
   - recipient_id: Phone number of the user with country code without +
-  - opts: Keyword list with the following options:
+  - opts: Map with the following options:
     - sticker: Sticker id or link of the sticker
     - recipient_type: Type of the recipient, either individual or group (default: "individual")
     - link: Whether to send a sticker id or a sticker link, true means that the sticker is a link, false means that the sticker is an id (default: true)
@@ -108,13 +108,13 @@ Sends a sticker message to a WhatsApp user.
 
 ## Example
 
-    iex> WhatsappElixir.send_sticker("5511999999999", sticker: "170511049062862", link: false)
+    iex> WhatsappElixir.send_sticker("5511999999999", %{sticker: "170511049062862", link: false})
     {:ok, %{"success" => true}}
 """
-def send_sticker(recipient_id, [sticker: sticker] = opts) do
-  recipient_type = Keyword.get(opts, :recipient_type, "individual")
-  link = Keyword.get(opts, :link, true)
-  custom_config = Keyword.get(opts, :custom_config, [])
+def send_sticker(recipient_id, %{sticker: sticker} = opts) do
+  recipient_type = Map.get(opts, :recipient_type, "individual")
+  link = Map.get(opts, :link, true)
+  custom_config = Map.get(opts, :custom_config, [])
 
   data = %{
     messaging_product: "whatsapp",
@@ -140,19 +140,19 @@ Sends an audio message to a WhatsApp user.
 
 ## Args
   - recipient_id: Phone number of the user with country code without +
-  - opts: Keyword list with the following options:
+  - opts: Map with the following options:
     - audio: Audio id or link of the audio
     - link: Whether to send an audio id or an audio link, true means that the audio is a link, false means that the audio is an id (default: true)
     - custom_config: Custom configuration for HTTP client (default: [])
 
 ## Example
 
-    iex> WhatsappElixir.send_audio("5511999999999", audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+    iex> WhatsappElixir.send_audio("5511999999999", %{audio: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"})
     {:ok, %{"success" => true}}
 """
-def send_audio(recipient_id, [audio: audio] = opts) do
-  link = Keyword.get(opts, :link, true)
-  custom_config = Keyword.get(opts, :custom_config, [])
+def send_audio(recipient_id, %{audio: audio} = opts) do
+  link = Map.get(opts, :link, true)
+  custom_config = Map.get(opts, :custom_config, [])
 
   data = %{
     messaging_product: "whatsapp",
@@ -177,7 +177,7 @@ Sends a video message to a WhatsApp user.
 
 ## Args
   - recipient_id: Phone number of the user with country code without +
-  - opts: Keyword list with the following options:
+  - opts: Map with the following options:
     - video: Video id or link of the video
     - caption: Caption of the video (default: "")
     - link: Whether to send a video id or a video link, true means that the video is a link, false means that the video is an id (default: true)
@@ -185,13 +185,13 @@ Sends a video message to a WhatsApp user.
 
 ## Example
 
-    iex> WhatsappElixir.send_video("5511999999999", video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    iex> WhatsappElixir.send_video("5511999999999", %{video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"})
     {:ok, %{"success" => true}}
 """
-def send_video(recipient_id, [video: video] = opts) do
-  caption = Keyword.get(opts, :caption, "")
-  link = Keyword.get(opts, :link, true)
-  custom_config = Keyword.get(opts, :custom_config, [])
+def send_video(recipient_id, %{video: video} = opts) do
+  caption = Map.get(opts, :caption, "")
+  link = Map.get(opts, :link, true)
+  custom_config = Map.get(opts, :custom_config, [])
 
   data = %{
     messaging_product: "whatsapp",
@@ -216,7 +216,7 @@ Sends a document message to a WhatsApp user.
 
 ## Args
   - recipient_id: Phone number of the user with country code without +
-  - opts: Keyword list with the following options:
+  - opts: Map with the following options:
     - document: Document id or link of the document
     - caption: Caption of the document (default: "")
     - link: Whether to send a document id or a document link, true means that the document is a link, false means that the document is an id (default: true)
@@ -224,13 +224,13 @@ Sends a document message to a WhatsApp user.
 
 ## Example
 
-    iex> WhatsappElixir.Media.send_document("5511999999999", document: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
+    iex> WhatsappElixir.Media.send_document("5511999999999", %{document: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"})
     {:ok, %{"success" => true}}
 """
-def send_document(recipient_id, [document: document] = opts) do
-  caption = Keyword.get(opts, :caption, "")
-  link = Keyword.get(opts, :link, true)
-  custom_config = Keyword.get(opts, :custom_config, [])
+def send_document(recipient_id, %{document: document} = opts) do
+  caption = Map.get(opts, :caption, "")
+  link = Map.get(opts, :link, true)
+  custom_config = Map.get(opts, :custom_config, [])
 
   data = %{
     messaging_product: "whatsapp",
